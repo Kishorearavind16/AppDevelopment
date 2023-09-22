546dfb2d5372810517e33d968b9752a750500351
Kishorearavind16 <122795187+Kishorearavind16@users.noreply.github.com>
2023-09-22 23:06:08 +0530

    Delete Day-05 directory

--- Day-05/src/Pages/index.js
+++
@@ -1,6 +0,0 @@
-import About from "./About/About";
-import Error from "./Error/Error";
-import Home from "./Home/Home";
-import ProductList from "./Products/ProductList";
-
-export { About, Error, Home, ProductList };
